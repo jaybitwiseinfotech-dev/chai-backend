26bb4085daa1bb7faf6c393a3a64520c520be371
jaybitwiseinfotech-dev <jay.bitwiseinfotech@gmail.com>
2026-08-29 10:44:20 +0530

    all setup configure

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 import dotenv from "dotenv"
-
-// import mongoose from "mongoose";
-// import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
-
+import {app} from './app.js'
 dotenv.config({
-    path:'./env'
+    path: './.env'
 })
+
 
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log(`Server is running at port:${process.env.PORT}`);
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
-.catch((err)=>{
-    console.log("MONGO db connection failed !!!", err)
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
-
 
 
 
